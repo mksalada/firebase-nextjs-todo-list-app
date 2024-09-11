5b04a5607bc4f917c5bca9f3e5be58e955c6ff0f
mksalada <mksalada@outlook.com>
2024-09-11 17:06:55 +0800

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,8 @@ import ListItems from "../components/ListItems";
 export default function Home() {
   return (
     <div className="bg-white h-screen w-screen text-black flex flex-col justify-center items-center">
-      <h1>NextJS Firebase Firestore</h1>
+      <h1>Todo List App</h1>
+      <small>Made with NextJS, Firebase and Firestore</small>
       <AddItem />
       <ListItems />
     </div>
